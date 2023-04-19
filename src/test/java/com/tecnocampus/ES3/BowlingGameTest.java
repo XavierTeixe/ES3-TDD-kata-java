f252e308dc4e73573e849da710496471d8745f26
XavierTeixe <102961315+XavierTeixe@users.noreply.github.com>
2023-04-19 12:48:25 +0200

    Commit 3

--- src/test/java/com/tecnocampus/ES3/BowlingGameTest.java
+++ src/test/java/com/tecnocampus/ES3/BowlingGameTest.java
@@ -12,4 +12,9 @@ public class BowlingGameTest {
     public void create_game() {
         var g = new Game();
     }
+    @Test
+    public void roll_a_ball() {
+        var g = new Game();
+        g.roll(0);
+    }
 }
